4f09a200d4568e0e169a3feb87b25e5c6212c253
borsh130894 <borsh130894@gmail.com>
2025-06-07 17:55:06 +0600

    Added general app, models, pillow

---
+++ backend/general/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from general.models import Note, Achievement, Advertisement
+
+
+admin.site.register(Note)
+admin.site.register(Achievement)
+admin.site.register(Advertisement)
---
+++ backend/general/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+
+
+class User(AbstractBaseUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    crated_by = models.ForeignKey(
+        "User", on_delete=models.CASCADE, verbose_name="created by"
+    )
+
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=30)
+    condition = models.TextField()
+    icon = models.ImageField()
+
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=225)
+    image = models.ImageField()
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
