310b5953fba3feb50bdb3fd0f9fad24d92eda1c6
borsh130894 <borsh130894@gmail.com>
2025-06-09 10:57:20 +0600

    Added service layer

---
+++ backend/event_feed/service/get_event_feed.py
@@ -0,0 +1,32 @@
+from django.db.models import CharField, Value, F
+from event_feed.serializers import Entity
+
+
+def get_event_feed(
+        models: dict, user_id: int, event_type: str, search: str
+) -> list:
+    result_list = []
+
+    for model, fields in models.items():
+        entity_name = model.__name__.lower()
+
+        records = model.objects.annotate(
+            entity=Value(
+                entity_name,
+                output_field=CharField()
+            ),
+            head=F('achievement__title') if 'achievement' in fields else F('title')
+        ).order_by('-created_at')
+
+        if entity_name == Entity.Note.value:
+            records = records.filter(created_by=user_id)
+        elif entity_name == Entity.Achievement.value:
+            records = records.filter(user_id=user_id)
+
+        if event_type:
+            records = records.filter(entity__in=['advertisement', event_type])
+        if search:
+            records = records.filter(head__icontains=search)
+
+        result_list += list(records)
+    return sorted(result_list, key=lambda x: x.created_at, reverse=True)
--- backend/event_feed/views.py
+++ backend/event_feed/views.py
@@ -3,43 +3,8 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from event_feed.models import Note, Advertisement, User, UserAchievement
-from typing import List, Dict
-from django.db.models import CharField, Value, F
-from event_feed.serializers import EventFeedSerializer, Entity
-
-
-def get_event_feed(
-        models: dict,
-        user_id: int,
-        event_type: str,
-        search: str
-) -> List[Dict]:
-    result_list = []
-
-    for model, fields in models.items():
-        entity_name = model.__name__.lower()
-
-        records = model.objects.annotate(
-            entity=Value(
-                entity_name,
-                output_field=CharField()
-            ),
-            head=F('achievement__title') if 'achievement' in fields else F('title')
-        ).order_by('-created_at')
-
-        if entity_name == Entity.Note.value:
-            records = records.filter(created_by=user_id)
-        elif entity_name == Entity.Achievement.value:
-            records = records.filter(user_id=user_id)
-
-        if event_type:
-            records = records.filter(entity__in=['advertisement', event_type])
-        if search:
-            records = records.filter(head__icontains=search)
-
-        result_list += list(records)
-
-    return sorted(result_list, key=lambda x: x.created_at, reverse=True)
+from event_feed.serializers import EventFeedSerializer
+from event_feed.service.get_event_feed import get_event_feed
 
 
 class EventFeedAPIView(APIView):
